dce2d1f70ef05264377bedf4af8eb4c24a2446d1
shtr_t <s.takeuchi@about.me>
2019-12-13 10:17:44 +0900

    added vue on gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_113635) do
+ActiveRecord::Schema.define(version: 2019_06_11_093514) do
 
   create_table "records", force: :cascade do |t|
     t.datetime "hour"
